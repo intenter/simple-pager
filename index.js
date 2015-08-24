53aab6c30e8dd9c6e8fec85c2e5719ad37a5a096
intenter <int@intenter.org>
2015-08-25 01:07:44 +0300

    small performance optimization

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ module.exports = {
         options = options ? options : {surroundWith:1};
         var model = [{page:currPage, current:true}];
         var testPage;
-        for (var i=0; i<options.surroundWith; i++) {
+        var i;
+
+        for (i=0; i<options.surroundWith; i++) {
             testPage = currPage + i + 1;
             if (testPage <= totalPages) {
                 model.push({page:testPage});
+            } else {
+                break;
             }
+        }
 
+        for (i=0; i<options.surroundWith; i++) {
             testPage = currPage -i - 1;
             if (testPage >= 1) {
                 model.unshift({page:testPage});
+            } else {
+                break;
             }
         }
         return model;
