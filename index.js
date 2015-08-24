5d09d264c9d43150698784eab440482b0fcae18d
intenter <int@intenter.org>
2015-08-25 01:03:04 +0300

    support different number of surrounding pages

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 module.exports = {
-    getPagerModel: function getPagerModel(currPage, totalPages) {
+    getPagerModel: function getPagerModel(currPage, totalPages, options) {
+        options = options ? options : {surroundWith:1};
         var model = [{page:currPage, current:true}];
         var testPage;
-        for (var i=0; i<1; i++) {
+        for (var i=0; i<options.surroundWith; i++) {
             testPage = currPage + i + 1;
             if (testPage <= totalPages) {
                 model.push({page:testPage});
--- test/pagerTest.js
+++ test/pagerTest.js
@@ -18,3 +18,24 @@ describe ('pager should work with default options', function(){
         expect(pager.getPagerModel(4, 10)).to.be.deep.equal([{page:3}, {page:4, current:true}, {page:5}]);
     });
 });
+
+describe ('pager can have different number of surrounding pages to display', function(){
+    it('works with the explicit single surrounding page', function(){
+        expect(pager.getPagerModel(4, 10, {surroundWith:1})).to.be.deep.equal([{page:3}, {page:4, current:true}, {page:5}]);
+    });
+    it('works with two surrounding pages', function(){
+        expect(pager.getPagerModel(4, 10, {surroundWith:2})).to.be.deep.equal([
+            {page:2}, {page:3},
+            {page:4, current:true},
+            {page:5}, {page:6}]);
+    });
+    it('works with more surrounding pages then available', function(){
+        expect(pager.getPagerModel(3, 5, {surroundWith:30})).to.be.deep.equal([
+            {page:1}, {page:2},
+            {page:3, current:true},
+            {page:4}, {page:5}]);
+    });
+    it('works with zero surrounding pages', function(){
+        expect(pager.getPagerModel(3, 5, {surroundWith:0})).to.be.deep.equal([{page:3, current:true}]);
+    });
+});
